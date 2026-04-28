b1e45daa5255ad306659ae608fbde6ab45741ed9
keethums7 <keethums@proton.me>
2026-04-27 21:11:06 -0700

    Updated coloring of icons and intro'd uptime calculation.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+
+window.addEventListener("load", () => {
+	// calculate total uptime first
+	const startDate = new Date(2026, 3, 22);
+	const timeDiff = Date.now() - startDate;
+
+	// build the timestring 
+	// const yyyy = 
+
+	let uptime = document.querySelector("#uptime");
+	
+	uptime.innerHTML = (timeDiff);
+});
